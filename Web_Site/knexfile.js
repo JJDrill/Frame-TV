3238b382851396641d57f4d148bcfe5e203893bd
J. Drill <JJDrill@hotmail.com>
2018-02-26 18:53:48 -0700

    updates to PROD

--- Web_Site/knexfile.js
+++ Web_Site/knexfile.js
@@ -1,5 +1,15 @@
 module.exports = {
 
+  prod: {
+    client: 'pg',
+    connection: {
+    	host: 'localhost',
+    	user: 'postgres',
+    	password: 'password',
+    	database: 'Frame_TV_DB'
+    }
+  },
+  
   production: {
     client: 'pg',
     host: 'postgres://192.168.1.124/Frame_TV_DB',
