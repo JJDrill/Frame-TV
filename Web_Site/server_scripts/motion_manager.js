b14a13e7ab846556dbd41394cd1fd2d59a15e2fc
J. Drill <JJDrill@hotmail.com>
2018-05-20 10:57:44 -0600

    more motion monitor cleanup and improved debug

--- Web_Site/server_scripts/motion_manager.js
+++ Web_Site/server_scripts/motion_manager.js
@@ -1,11 +1,13 @@
 const db = require('../data/frame_tv_db');
-// var moment = require('moment');
 var cache = require('./local_cache');
 var tv = require('./tv_control');
 const motion_monitor = require('./motion_monitor');
 
-const DEBUG = true;
+const DEBUG = false;
 var wait_time = 1000;
+var tv_timeout = 0;
+var tv_timeout_motion_threshold = 0;
+var tv_motion_sensitivity = 0;
 var seconds_counter = 0;
 var motion_count = 0;
 var target_tv_mode = ""
@@ -13,61 +15,77 @@ var previous_target_tv_mode = ""
 
 setInterval(() => {
   seconds_counter += wait_time / 1000
-  var tv_timeout = cache.get_setting("TV Timeout")
-  var tv_timeout_motion_threshold = cache.get_setting("TV Timeout Motion Threshold")
-  var tv_motion_sensitivity = cache.get_setting("Motion Sensitivity")
+  tv_timeout = cache.get_setting("TV Timeout")
+  tv_timeout_motion_threshold = cache.get_setting("TV Timeout Motion Threshold")
+  tv_motion_sensitivity = cache.get_setting("Motion Sensitivity")
   alerts = cache.purge_montior_alerts()
   alerts.forEach(function(item) {
-    console.log('Found motion alert: ' + item.TimeStamp + " / " + item.MotionDuration);
+    if (DEBUG) {
+      console.log('Found motion alert: ' + item.TimeStamp + " / " + item.MotionDuration);
+    }
     log_motion_detection(item.TimeStamp, item.MotionDuration)
   })
 
   previous_target_tv_mode = target_tv_mode
   target_tv_mode = cache.get_setting("Target TV Mode")
-  // console.log("target_tv_mode: ", target_tv_mode);
-  // console.log("current_tv_mode: ", current_tv_mode);
 
   if (target_tv_mode === "OFF") {
     StopMotionMonitoring();
-
-    if (previous_target_tv_mode != target_tv_mode) {
-      current_tv_mode = tv.Get_State(DEBUG);
-
-      if (current_tv_mode != target_tv_mode) {
-        console.log("Turning the TV Off.");
-        db.Add_Log(null, "TV OFF", "Turning the TV Off.").then()
-        tv.Turn_Off();
-      }
-    }
-
+    Verify_TV_Is_Off();
   } else if (target_tv_mode === "ON") {
     StopMotionMonitoring();
-    current_tv_mode = tv.Get_State(DEBUG);
-
-    if (current_tv_mode != target_tv_mode) {
-      console.log("Turning the TV On.");
-      db.Add_Log(null, "TV ON", "Turning the TV On.").then()
-      tv.Turn_On();
-    }
-
+    Verify_TV_Is_On();
   } else if (target_tv_mode === "MOTION") {
     StartMotionMonitoring();
-
-    if (seconds_counter >= tv_timeout) {
-
-      if (motion_count < tv_timeout_motion_threshold) {
-        current_tv_mode = tv.Get_State(DEBUG);
-        db.Add_Log(null, "TV OFF", "Turning the TV off due to lack of motion: " + motion_count).then()
-        tv.Turn_Off();
-      } else {
-        // console.log("Keeping the TV on: " + motion_count + "\n");
-      }
-      seconds_counter = 0
-      motion_count = 0
-    }
+    Verify_TV_Is_Monitoring_Motion();
   }
 }, wait_time);
 
+
+function Verify_TV_Is_Off(){
+  if (previous_target_tv_mode != target_tv_mode) {
+    current_tv_mode = tv.Get_State(DEBUG);
+
+    if (current_tv_mode != target_tv_mode) {
+      if (DEBUG) { console.log("Turning the TV Off."); }
+      db.Add_Log(null, "TV OFF", "Turning the TV Off.").then()
+      tv.Turn_Off();
+    }
+  }
+}
+
+function Verify_TV_Is_On(){
+  current_tv_mode = tv.Get_State(DEBUG);
+
+  if (current_tv_mode != target_tv_mode) {
+    if (DEBUG) { cconsole.log("Turning the TV On."); }
+    db.Add_Log(null, "TV ON", "Turning the TV On.").then()
+    tv.Turn_On();
+  }
+}
+
+function Verify_TV_Is_Monitoring_Motion(){
+  if (seconds_counter >= tv_timeout) {
+
+    if (motion_count < tv_timeout_motion_threshold) {
+      current_tv_mode = tv.Get_State(DEBUG);
+
+      if (current_tv_mode != target_tv_mode) {
+        message = "Turning the TV off due to lack of motion. " +
+        "(Motion Count: " + motion_count + ")"
+        if (DEBUG) { console.log(message); }
+        db.Add_Log(null, "TV OFF", message).then()
+        tv.Turn_Off();
+      }
+    } else {
+      message = "Keeping the TV on. (Motion Count: " + motion_count + ")"
+      if (DEBUG) { console.log(message); }
+      db.Add_Log(null, "TV ON", message).then()
+    }
+    seconds_counter = 0
+    motion_count = 0
+  }
+}
 
 function log_motion_detection(time_stamp, time_duration){
   motion_sensitivity = cache.get_setting("Motion Sensitivity")
--- Web_Site/server_scripts/settings_cache.js
+++ Web_Site/server_scripts/settings_cache.js
@@ -2,6 +2,7 @@
   This script will pull all the settings from the DB and populate
   them into the local_cache every WAIT_TIME seconds.
 */
+const DEBUG = false;
 const db = require('../data/frame_tv_db');
 const WAIT_TIME = 10000;
 var cache = require('./local_cache');
@@ -25,12 +26,14 @@ function Update_Cache(){
       cache.set_setting("Target TV Mode", data)
     })
   }).then(function(){
-    // console.log("\nUpdating settings...");
-    // console.log("************************************");
-    // settings_cache = cache.get_settings()
-    // for (var key in settings_cache) {
-    //   console.log(key + ": " + settings_cache[key]);
-    // }
-    // console.log("************************************\n");
+    if (DEBUG) {
+      console.log("\nUpdating settings...");
+      console.log("************************************");
+      settings_cache = cache.get_settings()
+      for (var key in settings_cache) {
+        console.log(key + ": " + settings_cache[key]);
+      }
+      console.log("************************************\n");
+    }
   })
 }
